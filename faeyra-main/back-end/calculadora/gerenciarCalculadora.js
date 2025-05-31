419aff510ba956fbcd4cd7552eef7bb710a33798
ellamylley <mor3@aluno.ifal.edu.br>
2025-05-31 16:44:16 -0300

    Animação do personagem adicionada

--- faeyra-main/back-end/calculadora/gerenciarCalculadora.js
+++ faeyra-main/back-end/calculadora/gerenciarCalculadora.js
@@ -3,8 +3,8 @@
 document.getElementById("abrirCalculadora").addEventListener("click", function () {
   const container = document.getElementById("calculadora");
   if (container.style.display === "none" || container.style.display === "") {
-  container.style.display = "flex";
-}
+    container.style.display = "flex";
+  }
 });
 
 
@@ -12,19 +12,4 @@ document.getElementById("abrirCalculadora").addEventListener("click", function (
 document.getElementById("fecharCalculadora").addEventListener("click", function () {
   document.getElementById("calculadora").style.display = "none";
 });
-
-function moverPersonagem() {
-  const container = document.getElementById("personagem");
-  let posicao = 150; 
-  const destino = 2000; 
-
-  const intervalo = setInterval(() => {
-    posicao += 5; // velocidade (px por "passo")
-    container.style.left = posicao + "px";
-
-    if (posicao >= destino) {
-      clearInterval(intervalo); // para quando chegar
-    }
-  }, 1); // a cada 20 milissegundos
-}
 
---
+++ faeyra-main/back-end/front-functions/personagemFunctions.js
@@ -0,0 +1,40 @@
+//Função que move personagem
+function moverPersonagem(entrarOuSair) {
+  const container = document.getElementById("personagem");
+  const botaoIniciar = document.getElementById("start");
+  const botaoFinalizar = document.getElementById("end");
+  let acao = entrarOuSair;
+  let posicao;
+  let destino;
+
+  if (acao === "entrar") {
+    posicao = -150;
+    destino = 150;
+    botaoIniciar.style.display = "none";
+    botaoFinalizar.style.display = "flex";
+  }
+
+  if (acao === "sair") {
+    posicao = 150;
+    destino = 2000;
+    botaoFinalizar.style.display = "none";
+    botaoIniciar.style.display = "flex";
+  }
+
+  let frame = 0;
+
+  const intervalo = setInterval(() => {
+    posicao += 3; 
+    frame++;
+
+    let deslocamentoVertical = Math.sin(frame * 0.2) * 5; // movimento vertical
+    container.style.left = posicao + "px";
+    container.style.top = `calc(20vh + ${deslocamentoVertical}px)`; // base + variação
+
+    if (posicao >= destino) {
+      clearInterval(intervalo);
+    }
+  }, 15);
+}
+
+
